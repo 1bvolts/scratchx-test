890b21fcfc6c0d17f765d5e716f353d17b0b1f3d
1bvolts <1bvolts@gmail.com>
2015-07-04 01:37:02 +0900

    containsブロックを追加

--- extension.js
+++ extension.js
@@ -16,12 +16,18 @@
         return str.substring(pos1 - 1, pos2);
     };
 
+    ext.contains = function(str1, str2) {
+        return str1.indexOf(str2) >= 0;
+    };
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             // Block type, block name, function name, param1 default value, param2 default value
             ['r', '%s にあいさつ', 'greeting', '名前'],
             ['r', '%s の %n 文字めから %n 文字めまで', 'mysubstring', '文字列', 1, 2]
+            ['r', '%s に %s が含まれる', 'contains', '文字列1', '文字列2']
+
         ]
     };
 
