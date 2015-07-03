0836655166162984ce703312bf86885bffb32535
1bvolts <1bvolts@gmail.com>
2015-07-03 15:40:57 +0900

    substringを追加

--- extension.js
+++ extension.js
@@ -12,14 +12,19 @@
         return 'こんにちは、' + name + '!';
     };
 
+    ext.mysubstring = function(str, pos1, pos2) {
+        return str.substring(pos1 - 1, pos2);
+    };
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             // Block type, block name, function name, param1 default value, param2 default value
             ['r', '%s にあいさつ', 'greeting', '名前'],
+            ['r', '$s の %n 文字めから %n 文字めまで', 'mysubstring']
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('greeting', descriptor, ext);
+    ScratchExtensions.register('test', descriptor, ext);
 })({});
